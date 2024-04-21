df624562cd6b5a257190dd5bb9f5a185d36cd44c
ioanamaria152317 <123132707+ioanamaria152317@users.noreply.github.com>
2024-04-21 16:29:57 +0300

    Bonus

---
+++ Ore.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class Ore {
+//    Given a graph,
+//            1.
+//    Step 0. Arrange the vertices in a circle.
+//
+//            2.
+//    Step 1. Look around the boundary, say in the counterclockwise direction,
+//    for consecutive nonadjacent vertices, i.e., a gap. If there are no gaps,
+//    quit with the spanning cycle on the boundary. Otherwise, look for a pair of crossing
+//    chords from the vertices of the gap to some other pair of consecutive vertices
+//    that may or may not be adjacent (possible gap 2).
+//
+//    If found, (i.e., gap 1 was good!), simply rearrange the circular order of the
+//    vertices in the obvious way so that the two chords become edges on the boundary and
+//    the gaps are switched to the interior. Each time we play this game of criss-cross
+//    successfully, one or two gaps on the boundary of the circular arrangement of vertices
+//    are replaced by two edges. Otherwise repeat Step 1 with the next gap.
+//
+//    Continue until the spanning cycle is on the boundary, or until every gap is bad.
+    
+
+}
